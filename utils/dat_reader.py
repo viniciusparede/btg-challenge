d7ac7bc6a703d76ed06befce82b1caff5a429047
Vinicius Parede <vinicius.parede1@gmail.com>
2023-08-16 18:13:12 -0300

    leitura dos arquivos paralelamente

--- utils/dat_reader.py
+++ utils/dat_reader.py
@@ -1,10 +1,32 @@
+import os
+import multiprocessing
+from concurrent.futures import ThreadPoolExecutor
+
 import pandas as pd
 
 
-def read_data_file(file_path: str) -> pd.DataFrame:
-    with open(file_path, "r") as f:
-        raw_file = f.readlines()
+def read_dat_file_to_dataframe(file_path: str) -> pd.DataFrame:
+    if not file_path[-4:] == ".bln":
+        with open(file_path, "r") as f:
+            raw_file = f.readlines()
 
-    list_dados = [line.split() for line in raw_file]
-    float_raw_lines = [list(map(float, raw_line)) for raw_line in list_dados]
-    return pd.DataFrame(float_raw_lines, columns=["lat", "long", "data_value"])
+        list_dados = [line.split() for line in raw_file]
+        float_raw_lines = [list(map(float, raw_line)) for raw_line in list_dados]
+        df = pd.DataFrame(float_raw_lines, columns=["lat", "long", "data_value"])
+        df["file_path"] = file_path
+        return df
+
+
+def multithreading_reader_data_file(folder_path: str) -> pd.DataFrame:
+    file_paths = [
+        os.path.join(folder_path, filename) for filename in os.listdir(folder_path)
+    ]
+    num_files = len(file_paths)
+    num_cpus = multiprocessing.cpu_count()
+    num_workers = min(num_files, num_cpus)
+
+    with ThreadPoolExecutor(max_workers=num_workers) as executor:
+        dfs = executor.map(read_dat_file_to_dataframe, file_paths)
+
+    combined_df = pd.concat(dfs, ignore_index=True)
+    return combined_df
